740c018bcb2987ddf96680e2e70302b56e1659af
Isabella W <isabellawindhaber@gmail.com>
2022-11-09 15:22:23 +0100

    ..

---
+++ ISABELLA/Challenge/actions/db_connect.php
@@ -0,0 +1,10 @@
+<?php 
+
+$hostname = "127.0.0.1"; // this is the hostname that you can find in the PhpMyAdmin and you can write "localhost" too
+$username = "root"; // be default the userName for the databases is root
+$password = ""; // by default there is no password in the databases
+$dbname = "restaurant"; // here we need to write the Database name
+
+// create connection, you need to be aware of the order of the parameters
+$connect = new mysqli($hostname, $username, $password, $dbname);
+?>
